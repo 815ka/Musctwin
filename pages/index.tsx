d0d8715bc084750f4b400ef1139d1a06ff31f4ca
815ka <815kas@gmail.com>
2025-07-01 08:24:41 +0400

    Add files via upload
    Signed-off-by: 815ka <815kas@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,4 @@
+// MusicTwin home page (placeholder)
+export default function Home() {
+  return <div>MusicTwin Home Page</div>;
+}
---
+++ pages/result.tsx
@@ -0,0 +1,4 @@
+// MusicTwin result page (placeholder)
+export default function Result() {
+  return <div>Generated Music Result</div>;
+}
